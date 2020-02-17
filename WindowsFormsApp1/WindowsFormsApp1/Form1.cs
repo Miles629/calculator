682ad78606f5ef70541696b8cafb82c55c2fa916
Miles <55200699+Miles629@users.noreply.github.com>
2020-02-17 19:24:49 +0800

    界面计算器，锦上添花版，移除所有bug。v1.0

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -18,7 +18,8 @@ namespace WindowsFormsApp1
         }
         private int haveop=0;
         private int havere=0;
-        private int haven1 = 0;
+        private int haven1 = 0,haven2 = 0;
+       
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -29,6 +30,7 @@ namespace WindowsFormsApp1
             string n1 = label1.Text;
             string n2 = label2.Text;
             string op = label5.Text;
+            //label4.Text = "=";
             double a1, a2;
             if (double.TryParse(n1, out a1) && double.TryParse(n2, out a2))
                 label3.Text = calculate(a1, a2, op).ToString();
@@ -37,6 +39,7 @@ namespace WindowsFormsApp1
             haveop = 0;
             haven1 = 0;
             havere = 1;
+            haven2 = 0;
         }
         private double calculate(double a1,double a2,string op)
         {
@@ -62,69 +65,112 @@ namespace WindowsFormsApp1
 
         private void button13_Click(object sender, EventArgs e)
         {
-            if(haven1==0&&havere==1)
+            if(haven1==1&&haveop==1&&haven2==1)//连续运算
+            {
+                string n1 = label1.Text;
+                string n2 = label2.Text;
+                string op = label5.Text;
+                double a1, a2;
+                if (double.TryParse(n1, out a1) && double.TryParse(n2, out a2))
+                    label1.Text = calculate(a1, a2, op).ToString();
+                else
+                    label3.Text = "NaN";
+            }
+            if(haven1==0&&havere==1)//一则运算过后立刻运算
             {
                 label1.Text = label3.Text;
-                label5.Text = "/";
-                haveop = 1;
             }
             else
             {
-                label5.Text = "/";
-                haveop = 1;
             }
+            label5.Text = "/";
+            haveop = 1;
             label2.Text = "";
 
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
+            if (haven1 == 1 && haveop == 1 && haven2 == 1)//连续运算
+            {
+                string n1 = label1.Text;
+                string n2 = label2.Text;
+                string op = label5.Text;
+                double a1, a2;
+                if (double.TryParse(n1, out a1) && double.TryParse(n2, out a2))
+                    label1.Text = calculate(a1, a2, op).ToString();
+                else
+                    label3.Text = "NaN";
+            }
             if (haven1 == 0 && havere == 1)
             {
                 label1.Text = label3.Text;
-                label5.Text = "*";
-                haveop = 1;
-                label2.Text = "";
             }
             else
             {
-                label5.Text = "*";
-                haveop = 1;
             }
+            label5.Text = "*";
+            haveop = 1;
             label2.Text = "";
 
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
+            if (haven1 == 1 && haveop == 1 && haven2 == 1)//连续运算
+            {
+                string n1 = label1.Text;
+                string n2 = label2.Text;
+                string op = label5.Text;
+                double a1, a2;
+                if (double.TryParse(n1, out a1) && double.TryParse(n2, out a2))
+                    label1.Text = calculate(a1, a2, op).ToString();
+                else
+                    label3.Text = "NaN";
+            }
             if (haven1 == 0 && havere == 1)
             {
                 label1.Text = label3.Text;
-                label5.Text = "-";
-                haveop = 1;
+                //label5.Text = "-";
+                //haveop = 1;
             }
             else
             {
-                label5.Text = "-";
-                haveop = 1;
+                //label5.Text = "-";
+                //haveop = 1;
             }
+            label5.Text = "-";
+            haveop = 1;
             label2.Text = "";
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
+            if (haven1 == 1 && haveop == 1 && haven2 == 1)//连续运算
+            {
+                string n1 = label1.Text;
+                string n2 = label2.Text;
+                string op = label5.Text;
+                double a1, a2;
+                if (double.TryParse(n1, out a1) && double.TryParse(n2, out a2))
+                    label1.Text = calculate(a1, a2, op).ToString();
+                else
+                    label3.Text = "NaN";
+            }
             if (haven1 == 0 && havere == 1)
             {
                 label1.Text = label3.Text;
-                label5.Text = "+";
-                haveop = 1;
+                //label5.Text = "+";
+                //haveop = 1;
                 
             }
             else
             {
-                label5.Text = "+";
-                haveop = 1;
+                //label5.Text = "+";
+                //haveop = 1;
             }
+            label5.Text = "+";
+            haveop = 1;
             label2.Text = "";
         }
 
@@ -135,9 +181,13 @@ namespace WindowsFormsApp1
                 label1.Text = "";
                 haven1 = 1;
             }
-            if(haveop == 0 && haven1 == 1)
+            if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "2";
-            else label2.Text = label2.Text + "2";
+            else
+            {
+                label2.Text = label2.Text + "2";
+                haven2 = 1;
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -149,7 +199,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "3";
-            else label2.Text = label2.Text + "3";
+            else
+            {
+                label2.Text = label2.Text + "3";
+                haven2 = 1;
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -161,7 +215,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "4";
-            else label2.Text = label2.Text + "4";
+            else
+            { 
+                label2.Text = label2.Text + "4";
+                haven2 = 1;
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -173,7 +231,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "5";
-            else label2.Text = label2.Text + "5";
+            else
+            { 
+                label2.Text = label2.Text + "5"; 
+                haven2 = 1; 
+            }
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -185,7 +247,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "6";
-            else label2.Text = label2.Text + "6";
+            else
+            {
+                label2.Text = label2.Text + "6";
+                haven2 = 1;
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -197,7 +263,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "7";
-            else label2.Text = label2.Text + "7";
+            else
+            {
+                label2.Text = label2.Text + "7";
+                haven2 = 1;
+            }
         }
 
         private void button8_Click(object sender, EventArgs e)
@@ -209,7 +279,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "8";
-            else label2.Text = label2.Text + "8";
+            else
+            {
+                label2.Text = label2.Text + "8";
+                haven2 = 1;
+            }
         }
 
         private void button9_Click(object sender, EventArgs e)
@@ -221,7 +295,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "9";
-            else label2.Text = label2.Text + "9";
+            else
+            {
+                label2.Text = label2.Text + "9";
+                haven2 = 1;
+            }
         }
 
         private void button10_Click(object sender, EventArgs e)
@@ -233,7 +311,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "0";
-            else label2.Text = label2.Text + "0";
+            else
+            {
+                label2.Text = label2.Text + "0";
+                haven2 = 1;
+            }
         }
 
         private void button16_Click(object sender, EventArgs e)
@@ -245,7 +327,47 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + ".";
-            else label2.Text = label2.Text + ".";
+            else
+            {
+                label2.Text = label2.Text + ".";
+                haven2 = 1;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (label6.Text == "怎么用都可以")
+                label6.Text = "就是功能不多";
+            else if (label6.Text == "就是功能不多")
+                label6.Text = "的高仿手机计算器";
+            else if (label6.Text == "的高仿手机计算器")
+                label6.Text = "支持用上次结果作为第一操作数";
+            else if (label6.Text == "支持用上次结果作为第一操作数")
+                label6.Text = "支持连续使用运算符--over";
+            else if (label6.Text == "支持连续使用运算符--over")
+                label6.Text = "怎么用都可以";
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (linkLabel1.LinkVisited == true)
+                linkLabel1.Text = "QMail:1067874054@qq.com";
+            try
+            {
+                linkLabel1.LinkVisited = true;
+                System.Diagnostics.Process.Start("https://github.com/Miles629/calculator");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+                linkLabel1.Text = "啊嘞，出错了？再次点击向作者反馈";
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -257,7 +379,11 @@ namespace WindowsFormsApp1
             }
             if (haveop == 0 && haven1 == 1)
                 label1.Text = label1.Text + "1";
-            else label2.Text=label2.Text+"1";
+            else
+            {
+                label2.Text = label2.Text + "1";
+                haven2 = 1;
+            }
         }
     }
 }
