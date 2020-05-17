4f2909be8d43b814952a486d98d55d36497e0ffe
Miles <55200699+Miles629@users.noreply.github.com>
2020-05-17 21:06:47 +0800

    第11次作业（待完善）

---
+++ homework11/homework11/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+  public class OrderContext : DbContext {
+
+    public OrderContext() : base("OrderDatabase") {
+      Database.SetInitializer(
+      new DropCreateDatabaseIfModelChanges<OrderContext>());
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Goods> GoodItems { get; set; }
+    public DbSet<Customer> Customers{ get; set; }
+
+  }
+}
---
+++ homework11/homework11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                using (var db = new OrderContext())
+                {
+                    var order = db.OrderItems.FirstOrDefault();
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
