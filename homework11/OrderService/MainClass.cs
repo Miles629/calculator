9dec106f62c3260acdb62face3fc9a1aa29d2810
Miles <55200699+Miles629@users.noreply.github.com>
2020-05-21 17:11:50 +0800

    第11次作业改动

---
+++ homework11/OrderService/MainClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp {
+
+  class MainClass {
+    public static void Main() {
+      try {
+
+        using (var db = new OrderContext()) {
+         var order=db.OrderItems.FirstOrDefault();
+         
+        }
+        
+
+      }catch (Exception e) {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
+      }
+
+    }
+  }
+}
