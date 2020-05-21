ee59eea357d83eb8b87de03e633467d8d53f16b1
Miles <55200699+Miles629@users.noreply.github.com>
2020-05-21 21:53:14 +0800

    更新了orderservice，按照老师的代码逻辑进行了函数修改

--- homework11/OrderService/OrderService.cs
+++ homework11/OrderService/OrderService.cs
@@ -11,43 +11,38 @@ namespace OrderApp {
    * The service class to manage orders
    * */
   public class OrderService {
-
+    public static OrderContext db=new OrderContext();
     public OrderService() {
     }
-
+    private static IQueryable<Order> getAllOrders()
+    {
+         return db.Orders.Include(o => o.Items.Select(i => i.GoodsItem))
+                 .Include("Customer");
+    }
     public static List<Order> GetAllOrders() {
-      using (var db = new OrderContext()) {
-        return AllOrders(db).ToList();
-      }
+        return getAllOrders().ToList();
     }
 
     public static Order GetOrder(string id) {
-      using (var db = new OrderContext()) {
-        return AllOrders(db).FirstOrDefault(o => o.Id == id);
-      }
+        return getAllOrders().FirstOrDefault(o => o.Id == id);
     }
 
     public static Order AddOrder(Order order) {
       try {
-        using (var db = new OrderContext()) {
           db.Orders.Add(order);
           db.SaveChanges();
-        }
-        return order;
+          return order;
       }
       catch (Exception e) {
-        //TODO 需要更加错误类型返回不同错误信息
         throw new ApplicationException($"添加错误: {e.Message}");
       }
     }
 
     public static void RemoveOrder(string id) {
       try {
-        using (var db = new OrderContext()) {
           var order = db.Orders.Include("Items").Where(o => o.Id == id).FirstOrDefault();
           db.Orders.Remove(order);
           db.SaveChanges();
-        }
       }
       catch (Exception e) {
         //TODO 需要更加错误类型返回不同错误信息
@@ -56,50 +51,37 @@ namespace OrderApp {
     }
 
     public static void UpdateOrder(Order newOrder) {
-      RemoveItems(newOrder.Id);
-      using (var db = new OrderContext()) {
+        RemoveItems(newOrder.Id);
         db.Entry(newOrder).State = EntityState.Modified;
         db.OrderItems.AddRange(newOrder.Items);
         db.SaveChanges();
-      }
     }
 
     private static void RemoveItems(string orderId) {
-      using (var db = new OrderContext()) {
         var oldItems = db.OrderItems.Where(item => item.OrderId == orderId);
         db.OrderItems.RemoveRange(oldItems);
         db.SaveChanges();
-      }
     }
 
     public static List<Order> QueryOrdersByGoodsName(string goodsName) {
-      using (var db = new OrderContext()) {
-        var query = AllOrders(db)
+        var query = getAllOrders()
           .Where(o => o.Items.Count(i => i.GoodsItem.Name == goodsName) > 0);
         return query.ToList();
-      }
     }
 
     public static List<Order> QueryOrdersByCustomerName(string customerName) {
-      using (var db = new OrderContext()) {
-        var query = AllOrders(db)
+        var query = getAllOrders()
           .Where(o => o.Customer.Name == customerName);
         return query.ToList();
-      }
     }
 
     public static object QueryByTotalAmount(float amout) {
-      using (var db = new OrderContext()) {
-        return AllOrders(db)
+        return getAllOrders()
           .Where(o => o.Items.Sum(item => item.GoodsItem.Price * item.Quantity) > amout)
           .ToList();
-      }
     }
 
-    private static IQueryable<Order> AllOrders(OrderContext db) {
-      return db.Orders.Include(o => o.Items.Select(i => i.GoodsItem))
-                .Include("Customer");
-    }
+
 
     public static void Export(String fileName) {
       XmlSerializer xs = new XmlSerializer(typeof(List<Order>));
@@ -121,9 +103,5 @@ namespace OrderApp {
         });
       }
     }
-
-
-
-
   }
 }
